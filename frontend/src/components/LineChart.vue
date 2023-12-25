5e44d433490f4b1e730cfe551208a7c4f696a1ec
yun-si <110306079@g.nccu.edu.tw>
2023-12-25 22:45:03 +0800

    style: 修改庫存管理頁面折線圖的外觀和功能

--- frontend/src/components/LineChart.vue
+++ frontend/src/components/LineChart.vue
@@ -4,13 +4,32 @@
   
 <script setup>
 import * as echarts from 'echarts/core';
-import { GridComponent } from 'echarts/components';
+import {
+    TitleComponent,
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    VisualMapComponent,
+    MarkAreaComponent,
+    LegendComponent
+} from 'echarts/components';
 import { LineChart } from 'echarts/charts';
 import { UniversalTransition } from 'echarts/features';
 import { CanvasRenderer } from 'echarts/renderers';
 
 // 使用相關模組
-echarts.use([GridComponent, LineChart, CanvasRenderer, UniversalTransition]);
+echarts.use([
+    TitleComponent,
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    VisualMapComponent,
+    MarkAreaComponent,
+    LegendComponent,
+    LineChart,
+    CanvasRenderer,
+    UniversalTransition
+]);
 
 import { onMounted, ref } from 'vue';
 
@@ -20,19 +39,54 @@ onMounted(() => {
     if (chartRef.value) {
         const myChart = echarts.init(chartRef.value);
         const option = {
+            // title: {
+            //     text: '過去銷售額及未來預測',
+            // },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross'
+                }
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    saveAsImage: {}
+                }
+            },
             xAxis: {
                 type: 'category',
                 data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
             },
             yAxis: {
-                type: 'value'
+                type: 'value',
+                axisPointer: {
+                    snap: true
+                }
+            },
+            legend: {  // 新增的 legend 配置
+                data: ['Sales', 'Forcast'],
             },
             series: [
                 {
+                    name: 'Forcast',
                     data: [150, 230, 224, 218, 135, 147, 260],
-                    type: 'line'
+                    type: 'line',
+                    color: 'rgba(255, 165, 0, 1)'
+                },
+                {
+                    name: 'Sales',
+                    data: [150, 230, 224, 218],
+                    type: 'line',
+                    color: 'rgba(0, 128, 255, 1)'
                 }
-            ]
+            ],
+            grid: {
+                left: 50,
+                top: 10,
+                right: 50,
+                bottom: 50
+            }
         };
         myChart.setOption(option);
     }
@@ -44,7 +98,5 @@ onMounted(() => {
 .chart-container {
     width: calc(100%-18%);
     height: 250px;
-    margin-top: 0;
-    padding-top: 0;
 }
 </style>
