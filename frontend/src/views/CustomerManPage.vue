f007b53723865fec158014a106d0619f80ea9e7c
frafishen <104518723+frafishen@users.noreply.github.com>
2023-12-28 01:39:04 +0800

    feat: connect the revenue and growth rate data in the CustomerManPage

--- frontend/src/views/CustomerManPage.vue
+++ frontend/src/views/CustomerManPage.vue
@@ -3,7 +3,7 @@
     <el-card class="box-card is-plain" v-if="currentRow">
         <template #header>
             <div class="card-header">
-                <span>客戶名稱: {{ currentRow.name }}</span>
+                <span>客戶名稱: {{ currentRow.client_name }}</span>
             </div>
         </template>
         <!-- 統計圖表 -->
@@ -16,15 +16,15 @@
     <div :class="table_part">
         <el-table ref="singleTableRef" :data="filterTableData" highlight-current-row style="width: 100%"
             @current-change="handleCurrentChange">
-            <el-table-column type="index" width="50" />
-            <el-table-column property="cus_id" label="ID" />
-            <el-table-column property="name" label="姓名" />
+            <!-- <el-table-column type="index" width="50" /> -->
+            <el-table-column property="client_id" label="客戶編號" />
+            <el-table-column property="client_name" label="姓名" />
             <el-table-column property="mail" label="email" />
-            <el-table-column property="date" label="最近購買日期" />
+            <el-table-column property="created_at" label="最近購買日期" />
             <el-table-column sortable property="clv" label="終身價值" />
             <el-table-column>
                 <template #header>
-                    <el-input v-model="search" size="small" placeholder="Type to search" /> <!--Search by name-->
+                    <el-input v-model="search" size="small" placeholder="Type to search" /> <!--Search by client_name-->
                 </template>
             </el-table-column>
         </el-table>
@@ -35,6 +35,111 @@
 </template>
 
 <script setup>
+import { ref, computed, onMounted, reactive } from 'vue'
+import { ElTable, ElCard } from 'element-plus'
+import BarChart from '@/components/BarChartWithLine.vue'
+import LineChart from '@/components/LineChartForCus.vue'
+import { fetchClients, fetchClientById, fetchCompanyRevenue, fetchRevenueGrowthRates, fetchAveragePurchaseInterval } from '@/api/CustomerManAPI' 
+
+const chartData_LHS = reactive({
+    xAxisData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    yAxisRev: [150, 230, 224, 218, 328, 400, 468],
+    yAxisGrowthRate: [10, 14, 13, 15, 9, 8, 7]
+})
+
+const chartData_RHS = reactive({
+    xAxisData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    yAxisInterval: [20, 16, 17, 14, 13, 9, 8],
+})
+
+const currentRow = ref()
+const singleTableRef = ref()
+const table_part = ref('table_part')
+const search = ref('')
+
+// Initialize tableData as a reactive reference
+const tableData = ref([])
+
+// Function to fetch clients and update tableData
+const loadClients = async () => {
+  try {
+    const clients = await fetchClients()
+    tableData.value = clients.map(client => ({
+      client_id: client.client_id.toString(),
+      client_name: client.client_name,
+      mail: client.email,
+      created_at: new Date(client.created_at).toLocaleDateString(),
+      clv: client.clv.toFixed(2)
+    }))
+  } catch (error) {
+    console.error('Error fetching client data:', error)
+  }
+}
+
+// Function to fetch company revenue and update chartData_LHS
+const loadCompany = async (clientId) => {
+  try {
+    // Fetch both revenue and growth rates concurrently
+    const [revenueResponse, growthRateResponse] = await Promise.all([
+      fetchCompanyRevenue(clientId),
+      fetchRevenueGrowthRates(clientId)
+    ]);
+
+    const revenueData = revenueResponse[0].revenue;
+    const growthRatesData = growthRateResponse[0]['growth rates'];
+
+    console.log(revenueData)
+    console.log(growthRatesData)
+
+    // Assuming both revenueData and growthRatesData have the same keys (months)
+    chartData_LHS.xAxisData = Object.keys(revenueData);
+
+    // Update yAxisRev and yAxisGrowthRate
+    chartData_LHS.yAxisRev = Object.values(revenueData);
+    chartData_LHS.yAxisGrowthRate = Object.values(growthRatesData);
+  } catch (error) {
+    console.error('Error fetching company data:', error);
+  }
+};
+
+// Call loadClients when the component is mounted
+onMounted(async () => {
+    await loadClients();
+    await loadCompany(2); 
+});
+
+// Filter function for the table
+const filterTableData = computed(() => 
+  tableData.value.filter(
+    data =>
+      !search.value ||
+      data.client_name.toLowerCase().includes(search.value.toLowerCase())
+  )
+)
+
+// Clear the current row selection
+const clear = () => {
+  currentRow.value = null
+  table_part.value = 'table_part'
+}
+
+const handleCurrentChange = async (val) => {
+  if (val && val.client_id) {
+    currentRow.value = val;
+    table_part.value = 'table_part_half';
+    // console.log(val.client_id)
+
+    // Call loadCompany with the client_id of the selected row
+    loadCompany(val.client_id);
+  }
+};
+
+</script>
+
+
+
+
+<!-- <script setup>
 import { ref, computed } from "vue"
 import { ElTable, ElCard } from 'element-plus'
 import BarChart from '@/components/BarChartWithLine.vue'
@@ -63,30 +168,30 @@ const filterTableData = computed(() =>
   tableData.filter(
     (data) =>
       !search.value ||
-      data.name.toLowerCase().includes(search.value.toLowerCase())
+      data.client_name.toLowerCase().includes(search.value.toLowerCase())
   )
 )
 
 const tableData = [
     {
-        cus_id: '10000001',
-        name: 'AAA',
+        client_id: '10000001',
+        client_name: 'AAA',
         mail: 'a@gmail.com',
-        date: '2023-11-02',
+        created_at: '2023-11-02',
         clv: 9835
     },
     {
-        cus_id: '10000002',
-        name: 'BBB',
+        client_id: '10000002',
+        client_name: 'BBB',
         mail: 'b@gmail.com',
-        date: '2023-11-01',
+        created_at: '2023-11-01',
         clv: 356
     },
     {
-        cus_id: '10000003',
-        name: 'CCC',
+        client_id: '10000003',
+        client_name: 'CCC',
         mail: 'c@gmail.com',
-        date: '2023-11-20',
+        created_at: '2023-11-20',
         clv: 98356
     }
 ];
@@ -104,7 +209,7 @@ const handleCurrentChange = (val) => {
 
 };
 
-</script>
+</script> -->
     
 <style scoped>
 .box-card {
